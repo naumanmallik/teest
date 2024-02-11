3bcaaa45d9ad7e366d3f899eb106a5ab4c51f9b1
Naveed Ul Aziz <naveed.dev01@invomail.io>
2024-02-11 17:32:26 +0000

    init

--- utils/cron-job.js
+++ utils/cron-job.js
@@ -1,16 +1,23 @@
-const cron = require("node-cron");
+// const cron = require("node-cron");
 const { uploadCloudinary } = require("./uploadCloudinary.util");
 
-const task = async () => {
-  try {
-    // const result = await uploadCloudinary("input3.jpg");
-    console.log("Cron job executed at:", new Date().toLocaleString());
-  } catch (error) {
-    console.log("error", error);
-  }
-};
+// const task = async () => {
+//   try {
+//     // const result = await uploadCloudinary("input3.jpg");
+//     console.log("Cron job executed at:", new Date().toLocaleString());
+//   } catch (error) {
+//     console.log("error", error);
+//   }
+// };
 
-// Schedule the cron job to run every 5 minutes
-cron.schedule("*/2 * * * *", task);
+// // Schedule the cron job to run every 5 minutes
+// cron.schedule("*/2 * * * *", task);
 
-console.log("Cron job scheduled to run every 5 minutes.");
+// console.log("Cron job scheduled to run every 5 minutes.");
+
+export default async function handler(req, res) {
+  console.log("Cron job executed at:", new Date().toLocaleString());
+  const result = await uploadCloudinary("input3.jpg");
+
+  res.status(200).send("Cron job completed");
+}
